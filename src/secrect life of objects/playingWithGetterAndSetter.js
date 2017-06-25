70fb028e09a3972ee882a8104261ae954c28a68d
abhsing@adobe.com <abhsing@adobe.com>
2017-06-25 16:09:03 +0530

    adding getter and setter files

--- src/secrect life of objects/playingWithGetterAndSetter.js
+++ src/secrect life of objects/playingWithGetterAndSetter.js
@@ -11,3 +11,17 @@ var obj = {
 console.log(obj.height);
 obj.height = 20;
 console.log(obj.height);
+
+Object.defineProperty(Object.getPrototypeOf(obj),"increaseHeight",{
+    get:function(){
+        this.h = this.h + this.val;
+        return this.h;
+    },
+    set:function(val){
+        this.val = val;
+    }
+});
+
+obj.increaseHeight = 30;
+console.log(obj.increaseHeight);
+console.log(obj.height);
